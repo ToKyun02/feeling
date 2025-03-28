82a5051cb7614ed3c9b1bcf1bc054fe48c039049
Tokyun02 <developtk02@gmail.com>
2025-03-28 17:25:28 +0900

    :wrench: chore : initial setting

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#0f7f80',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+const prettierConfig = {
+  printWidth: 120,
+  endOfLine: "auto",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  parser: "typescript",
+};
+
+export default prettierConfig;
